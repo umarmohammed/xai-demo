a5de868ff3cc33b960010bb2bd6a1ef418843e1d
Umar Mohammed <umar@teacherapp.co.uk>
2020-04-18 22:47:52 +0100

    show prediction probabilities
    closes #25

---
+++ src/xai-app/src/app/lime-probabilities.component.ts
@@ -0,0 +1,39 @@
+import { Component, Input } from '@angular/core';
+import { SingleChartResult } from './single-chart-result';
+
+@Component({
+  selector: 'xai-lime-probabilities',
+  template: `
+    <div class="container">
+      <h3 class="title">Prediction Probabilities</h3>
+      <ngx-charts-bar-horizontal
+        [results]="results"
+        [xScaleMax]="1"
+        [xScaleMin]="0"
+        [yAxis]="true"
+        [trimYAxisTicks]="false"
+        [showDataLabel]="true"
+        [scheme]="colorScheme"
+      >
+      </ngx-charts-bar-horizontal>
+    </div>
+  `,
+  styles: [
+    `
+      .container {
+        height: 50%;
+      }
+
+      .title {
+        text-align: center;
+      }
+    `,
+  ],
+})
+export class LimeProbabilitiesComponent {
+  @Input() results: SingleChartResult[];
+
+  colorScheme = {
+    domain: ['#1f77b4', '#ff7f0e'],
+  };
+}
