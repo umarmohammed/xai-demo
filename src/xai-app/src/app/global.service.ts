e5d05ecfb25b6dc0f01e92dd431dbf90b09c3d1c
Umar Mohammed <umar@teacherapp.co.uk>
2020-04-19 19:19:03 +0100

    use environment variable for url
    closes #50

--- src/xai-app/src/app/global.service.ts
+++ src/xai-app/src/app/global.service.ts
@@ -2,12 +2,12 @@ import { Injectable } from '@angular/core';
 import { ModelService } from './model.service';
 import { HttpClient } from '@angular/common/http';
 import { switchMap, map, filter } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class GlobalService {
-  private infoUrl = 'http://umarmohammed.io:8083/api/global/feature-info';
-  private importanceUrl =
-    'http://umarmohammed.io:8083/api/global/feature-importance';
+  private infoUrl = `${environment.baseUrl}api/global/feature-info`;
+  private importanceUrl = `${environment.baseUrl}api/global/feature-importance`;
 
   globalInfo$ = this.modelService.model$.pipe(
     switchMap((model) => this.http.post(this.infoUrl, model))
