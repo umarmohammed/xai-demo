72a7a4a5f765bca2a678bb023308cf059e8a8129
Umar Mohammed <umar@teacherapp.co.uk>
2020-04-20 16:56:00 +0100

    cache http responses
    closes #68

--- src/xai-app/src/app/data.service.ts
+++ src/xai-app/src/app/data.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { ModelService } from './model.service';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, shareReplay } from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { ModelData } from './model-data';
 import { environment } from 'src/environments/environment';
@@ -10,7 +10,8 @@ export class DataService {
   private uploadUrl = `${environment.baseUrl}api/upload`;
 
   data$ = this.modelService.model$.pipe(
-    switchMap((model) => this.http.post<ModelData>(this.uploadUrl, model))
+    switchMap((model) => this.http.post<ModelData>(this.uploadUrl, model)),
+    shareReplay()
   );
 
   constructor(private modelService: ModelService, private http: HttpClient) {}
--- src/xai-app/src/app/lime.service.ts
+++ src/xai-app/src/app/lime.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { DataGridService } from './data-grid.service';
 import { ModelService } from './model.service';
-import { withLatestFrom, switchMap, filter, map } from 'rxjs/operators';
+import {
+  withLatestFrom,
+  switchMap,
+  filter,
+  map,
+  shareReplay,
+  scan,
+} from 'rxjs/operators';
 import { HttpClient } from '@angular/common/http';
 import { createChartResult } from './single-chart-result';
 import { LimeResponse } from './lime-response';
 import { environment } from 'src/environments/environment';
+import { Observable, forkJoin } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -13,14 +21,25 @@ import { environment } from 'src/environments/environment';
 export class LimeService {
   private url = (id: string) => `${environment.baseUrl}api/lime/${id}`;
 
+  // Oh dear! Will probably use ngrx so cba
+  // with a proper scan implementation
+  cachedCalls = {};
+
   results$ = this.gridService.selectedRowId$.pipe(
     filter((id) => !!id),
     withLatestFrom(this.modelService.model$),
-    switchMap(([id, model]) =>
-      this.http
-        .post<LimeResponse>(this.url(id), model)
-        .pipe(map((res) => ({ ...res, exp: res.exp.map(createChartResult) })))
-    )
+    switchMap(([id, model]) => {
+      if (!this.cachedCalls[id]) {
+        this.cachedCalls[id] = this.http
+          .post<LimeResponse>(this.url(id), model)
+          .pipe(
+            map((res) => ({ ...res, exp: res.exp.map(createChartResult) })),
+            shareReplay()
+          );
+      }
+
+      return this.cachedCalls[id];
+    })
   );
 
   constructor(
