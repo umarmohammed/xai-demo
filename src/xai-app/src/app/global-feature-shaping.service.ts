276699067b02797a1019df8dadd9bca9b8385b50
umarmohammed <umar@thinkincode.co.uk>
2020-04-21 23:46:30 +0100

    draw feature shaping bar chart (#90)
    closes #89
    
    Co-authored-by: Umar Mohammed <umar@teacherapp.co.uk>

--- src/xai-app/src/app/global-feature-shaping.service.ts
+++ src/xai-app/src/app/global-feature-shaping.service.ts
@@ -2,40 +2,54 @@ import { Injectable } from '@angular/core';
 import { foo } from './foo';
 import { of } from 'rxjs';
 import { FeatureShapingData } from './feature-shaping-response';
-import { map } from 'rxjs/operators';
+import { map, shareReplay } from 'rxjs/operators';
 import { zip } from './array-utils';
 
 @Injectable({ providedIn: 'root' })
 export class GlobalFeatureShapingService {
-  globalFeatureShaping$ = of(foo);
+  globalFeatureShapingResponse$ = of(foo).pipe(shareReplay());
 
-  featureShaping$ = this.globalFeatureShaping$.pipe(
-    map((fs) => ({
-      results: this.featureShapingDataToNgxChartsSeries(fs.data),
-      title: fs.layout.title.text,
-      yScaleMin: fs.layout.yaxis.range[0],
-      yScaleMax: fs.layout.yaxis.range[1],
-      yAxisLabel: fs.layout.yaxis.title.text,
+  featureShapingLine$ = this.globalFeatureShapingResponse$.pipe(
+    map((reponse) => ({
+      results: this.featureShapingDataToNgxLineChart(reponse.data),
+      yScaleMin: reponse.layout.yaxis.range[0],
+      yScaleMax: reponse.layout.yaxis.range[1],
+      yAxisLabel: reponse.layout.yaxis.title.text,
     }))
   );
 
-  private featureShapingDataToNgxChartsSeries(data: FeatureShapingData[]) {
+  featureShapingBar$ = this.globalFeatureShapingResponse$.pipe(
+    map((response) => ({
+      results: this.featureShapingDataToNgxBarChart(response.data),
+      yAxisLabel: response.layout.yaxis2.title.text,
+    }))
+  );
+
+  private featureShapingDataToNgxBarChart(data: FeatureShapingData[]) {
+    const barData = data.find(this.isBarChart);
+
+    return barData.y.map((value, i) => ({ value, name: barData.x[i] }));
+  }
+
+  private featureShapingDataToNgxLineChart(data: FeatureShapingData[]) {
     return [
       {
-        name: data[1].name,
+        name: 'Main',
         series: zip(
           data.find(this.islineChart).x.map((v) => ({ name: v })),
-          ...data.filter(this.islineChart).map(this.getValues)
+          ...data.filter(this.islineChart).map(this.getLineChartValues)
         ),
       },
     ];
   }
 
-  private islineChart(data: FeatureShapingData) {
-    return data.type === 'scatter';
-  }
+  private isChartType = (type: string) => (data: FeatureShapingData) =>
+    data.type === type;
 
-  private getValues(data: FeatureShapingData) {
+  private islineChart = this.isChartType('scatter');
+  private isBarChart = this.isChartType('bar');
+
+  private getLineChartValues(data: FeatureShapingData) {
     const fs2NgxChartsMap = {
       'Lower Bound': 'min',
       'Upper Bound': 'max',
