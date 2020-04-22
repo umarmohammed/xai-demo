baf84dfabda2d8bc9d26b6e2ef2c322d949d9336
Umar Mohammed <umar@teacherapp.co.uk>
2020-04-22 14:59:18 +0100

    add upload text model button
    closes #103

--- src/xai-app/src/app/home.component.ts
+++ src/xai-app/src/app/home.component.ts
@@ -4,20 +4,25 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'xai-home',
-  template: ` <button
-      (click)="fileInput.click()"
-      color="primary"
-      mat-stroked-button
-    >
-      Upload Model
-    </button>
-    <input
-      hidden
-      type="file"
-      #fileInput
-      type="file"
-      (change)="fileUploaded(fileInput.files[0])"
-    />`,
+  template: `<div class="grid-container">
+    <div class="button-container">
+      <button (click)="fileInput.click()" color="primary" mat-stroked-button>
+        Upload Tabular Model
+      </button>
+      <input
+        hidden
+        type="file"
+        #fileInput
+        type="file"
+        (change)="fileUploaded(fileInput.files[0])"
+      />
+    </div>
+    <div class="button-container">
+      <button (click)="fileInput.click()" color="primary" mat-stroked-button>
+        Upload Text Model
+      </button>
+    </div>
+  </div> `,
   styleUrls: ['home.component.scss'],
 })
 export class HomeComponent {
