fa4e3bafb6989dfe629326cd164a1ee0d3c142d9
Umar Mohammed <umar@thinkincode.co.uk>
2020-04-28 18:43:08 +0100

    add netlify link to main page

--- src/xai-app/src/app/app.component.ts
+++ src/xai-app/src/app/app.component.ts
@@ -2,6 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<router-outlet></router-outlet>`,
+  template: `<router-outlet></router-outlet>
+
+    <a
+      style="position: absolute; left: 0; bottom: 0; z-index: 1;"
+      href="https://www.netlify.com"
+    >
+      <img
+        src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg"
+        alt="Deploys by Netlify"
+      />
+    </a>`,
 })
 export class AppComponent {}
